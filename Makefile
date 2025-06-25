01702435666207f22b48d5ebcc5bd1033efafa28
Joshua Raphael <josh.n.raphael@gmail.com>
2025-06-25 14:26:02 -0700

    fix project setup for unit tests

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ run:
 	dotnet run --project src/RASharp.csproj
 
 test:
-	dotnet run --project tests/RASharp.Tests.csproj
+	dotnet test
--- tests/Http/RequestTest.cs
+++ tests/Http/RequestTest.cs
@@ -13,7 +13,7 @@ namespace RASharp.Http.Tests
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            Assert.Fail();
         }
     }
 }
